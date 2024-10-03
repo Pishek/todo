179c6affe48699622461c1044990fbd6458e6817
p.afanasev <p.afanasev@x5.ru>
2024-10-03 05:21:58 +0300

    add dev depends

--- apps/todo/views.py
+++ apps/todo/views.py
@@ -1,5 +1,4 @@
 from rest_framework.generics import GenericAPIView
-from rest_framework.mixins import ListModelMixin
 from rest_framework.response import Response
 from rest_framework import serializers
 
