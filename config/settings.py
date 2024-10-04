c04eb6ef83aae090cf65d68088308b9c6eb0ede2
p.afanasev <p.afanasev@x5.ru>
2024-10-04 17:05:25 +0300

    change Token -> Bearer

--- config/settings.py
+++ config/settings.py
@@ -137,7 +137,7 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
-    "DEFAULT_AUTHENTICATION_CLASSES": ("rest_framework.authentication.TokenAuthentication",),
+    "DEFAULT_AUTHENTICATION_CLASSES": ("apps.todo.authentication.CustomBearerTokenAuthentication",),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
