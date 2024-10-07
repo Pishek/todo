eaaa47b275e2a90d55693afa420402ac81bec318
p.afanasev <p.afanasev@x5.ru>
2024-10-07 06:11:57 +0300

    minor refactoring

--- apps/todo/infra/kafka/producer.py
+++ apps/todo/infra/kafka/producer.py
@@ -1,10 +1,23 @@
+import json
+from typing import Any
+
 from confluent_kafka import Producer
 
+from apps.todo.infra.kafka.enums import TopicKafkaEnum
 from config.todo.settings import KAFKA_CONFIG
 
-producer = Producer(KAFKA_CONFIG)
+
+class KafkaProducer:
+    def __init__(self) -> None:
+        self._producer = Producer(KAFKA_CONFIG)
+
+    def send_completed_task(self, message: dict[str, Any]) -> None:
+        self.__send_message(topic=TopicKafkaEnum.COMPLETED_TASKS, message=message)
+
+    def __send_message(self, topic: str, message: dict[str, Any]) -> None:
+        self._producer.produce(topic, value=json.dumps(message))
+        self._producer.flush()
 
 
-def send_message(topic, message):  # type: ignore
-    producer.produce(topic, value=message)
-    producer.flush()
+def get_kafka_producer() -> KafkaProducer:
+    return KafkaProducer()
